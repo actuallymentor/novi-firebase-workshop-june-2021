f89e04ff504be2d0707a7c12e3cbb762d3da48a4
Mentor Palokaj <mentor@palokaj.co>
2021-06-09 21:04:49 +0200

    Workshop done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+
+import app from './firebase'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // State management
+  const [ action, setAction ] = useState( 'login' )
+  const [ email, setEmail ] = useState( '' )
+  const [ password, setPassword ] = useState( '' )
+  const [ user, setUser ] = useState()
+
+  // Handle the submit
+  async function onSubmit( e ) {
+
+    // Prevent page reload
+    e.preventDefault( )
+    console.log( `${ action } requested with email ${ email } and password ${ password }` )
+
+    // Do the actual registration
+    try {
+
+      if( action === 'login' ) {
+        const userCredential = await app.auth().signInWithEmailAndPassword( email, password )
+        console.log( 'Logged in: ', userCredential )
+        setUser( userCredential.user )
+      }
+
+      if( action === 'register' ) {
+        const userCredential = await app.auth().createUserWithEmailAndPassword( email, password )
+        console.log( 'Registered', userCredential )
+        setUser( userCredential.user )
+      }
+
+    } catch( e ) {
+      console.error( 'Firebase fail: ', e )
+    }
+
+  }
+
+  // There is a user logged in
+  if( user ) return <main>
+    
+    <h1>Hello { user.email }</h1>
+
+  </main>
+
+  // No user logged in
+  return <main>
+    
+    <h1>Hello Granny</h1>
+    <h2>{ action } here:</h2>
+
+    <form onSubmit={ onSubmit }>
+
+      <input onChange={ e => setEmail( e.target.value ) } placeholder='your@email.com' type='email' name='email' value={ email } />
+      <input onChange={ e => setPassword( e.target.value ) } placeholder='Your password' type='password' name='password' value={ password } />
+      <input type='submit' value={ action } />
+
+    </form>
+
+    <a id='logintoggle' href='#' onClick={ f => setAction( action == 'login' ? 'register' : 'login' ) }>{ action == 'login' ? 'Register' : 'Login' } instead</a>
+
+  </main>
+
 }
 
-export default App;
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// Firebase import
+import firebase from 'firebase'
+const app = firebase.initializeApp( {
+  apiKey: "AIzaSyDjrtharoe8fFmp862LhvEzJnvKSQ-leec",
+  authDomain: "granny-address-reminder-8a370.firebaseapp.com",
+  projectId: "granny-address-reminder-8a370",
+  storageBucket: "granny-address-reminder-8a370.appspot.com",
+  messagingSenderId: "115149301015",
+  appId: "1:115149301015:web:133deab26619def40b8352"
+} )
+
+export default app
